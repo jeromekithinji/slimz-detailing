e387bf592ed2b806097545cb6c2cbaf4a5bf23f6
jeromekithinji <kithinjijerome@gmail.com>
2023-11-22 16:13:05 +0300

    Nav bar responsive

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navBar.scss"
 import Logo from "../../assets/images/Logo.png"
+import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+
+
 
 const NavBar = () => {
-  return (
-    <div className="navBar">
-        <img src={Logo} alt="Slimz Detailing" className="navBar__logo" />
-        <div className="navBar__links">
-            <a href="#" className="navBar__link">Home</a>
-            <a href="#" className="navBar__link">Services</a>
-            <a href="#" className="navBar__link">Gallery</a>
-            <a href="#" className="navBar__link">Contact</a>
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleMenuToggle = () => {
+        setShowMenu(!showMenu);
+    };
+
+    return (
+        <div className="navBar">
+            <img src={Logo} alt="Slimz Detailing" className="navBar__logo" />
+            <div className={`navBar__links ${showMenu ? 'showMenu' : ''}`}>
+                <a href="#" className="navBar__link">Home</a>
+                <a href="#" className="navBar__link">Services</a>
+                <a href="#" className="navBar__link">Gallery</a>
+                <a href="#" className="navBar__link">Contact</a>
+            </div>
+            {showMenu ? (
+                <MdClose className="navBar__menuIcon" onClick={handleMenuToggle} />
+            ) : (
+                <FiMenu className="navBar__menuIcon" onClick={handleMenuToggle} />
+            )}
         </div>
-    </div>
-  )
+    )
 }
 
 export default NavBar
