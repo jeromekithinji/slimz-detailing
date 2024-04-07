603550a24fa1970a7a979c35b760a5b2a6790657
Jerome Kithinji <kithinjijerome@gmail.com>
2024-04-07 02:58:45 -0400

    Gallery page added

--- src/pages/galleryPage/galleryPage.jsx
+++ src/pages/galleryPage/galleryPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./galleryPage.scss"
 
+import galleryData from "../../assets/data/galleryData"
+
 
 const GalleryPage = () => {
   return (
@@ -8,6 +10,13 @@ const GalleryPage = () => {
         <p className="galleryPage-title">
             Gallery
         </p>
+        <div className="galleryPage__container">
+                {galleryData.map((item, index) => (
+                    <div className="galleryPage__container-item">
+                        <img src={galleryData[index].image} alt={galleryData[index].altText} className="galleryPage__container-item-image" />
+                    </div>
+                ))}
+        </div>
     </div>
   )
 }
