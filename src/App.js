a67e486c0afb3ba998574ac0b175bb3407bded9f
Jerome Kithinji <kithinjijerome@gmail.com>
2024-04-30 02:04:10 -0400

    Site navigation added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import ContactNav from './components/contactNav/contactNav';
-import NavBar from './components/navBar/navBar';
-import Footer from './components/footer/footer';
-import ContactFooter from './components/contactFooter/contactFooter';
-import HomePage from './pages/homePage/homePage';
-import ServicesPage from './pages/servicesPage/servicesPage';
-import GalleryPage from './pages/galleryPage/galleryPage';
+import logo from "./logo.svg";
+import "./App.css";
+import ContactNav from "./components/contactNav/contactNav";
+import NavBar from "./components/navBar/navBar";
+import Footer from "./components/footer/footer";
+import ContactFooter from "./components/contactFooter/contactFooter";
+import HomePage from "./pages/homePage/homePage";
+import ServicesPage from "./pages/servicesPage/servicesPage";
+import GalleryPage from "./pages/galleryPage/galleryPage";
+import { BrowserRouter as Router, Switch, Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-        <div className="App__header">
-            <ContactNav />
-            <NavBar />
-        </div>
-        <div className="App__content">
-            <HomePage />
-            {/* <ServicesPage /> */}
-            {/* <GalleryPage /> */}
-        </div>
-        <div className="App__footer">
-            <ContactFooter />
-            <Footer />
-        </div>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <div className="App__header">
+                    <ContactNav />
+                    <NavBar />
+                </div>
+                <div className="App__content">
+                <Routes>
+                    <Route index path="/" element={<HomePage />} />
+                    <Route path="/services" element={<ServicesPage />} />
+                    <Route path="/gallery" element={<GalleryPage />} />
+                </Routes>
+                </div>
+                <div className="App__footer">
+                    <ContactFooter />
+                    <Footer />
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -3,6 +3,7 @@ import "./navBar.scss"
 import Logo from "../../assets/images/Logo.png"
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
+import { Link } from 'react-router-dom';
 
 
 const NavBar = () => {
@@ -17,10 +18,19 @@ const NavBar = () => {
         <div className="navBar">
             <img src={Logo} alt="Slimz Detailing" className="navBar__logo" />
             <div className={`navBar__links ${showMenu ? 'showMenu' : ''}`}>
-                <a href="#" className="navBar__link">Home</a>
-                <a href="#" className="navBar__link">Services</a>
-                <a href="#" className="navBar__link">Gallery</a>
-                <a href="#" className="navBar__link">Contact</a>
+                {/* <a href="#" className="navBar__link">Home</a> */}
+                <Link to="/" className="navBar__link">
+                    Home
+                </Link>
+                <Link to="/services" className="navBar__link">
+                    Services
+                </Link>
+                <Link to="/gallery" className="navBar__link">
+                    Gallery
+                </Link>
+                <Link to="/contact" className="navBar__link">
+                    Contact
+                </Link>
             </div>
             {showMenu ? (
                 <MdClose className="navBar__menuIcon" onClick={handleMenuToggle} />
