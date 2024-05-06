58d456e9017e5dc5fd2df9d9f695de96498e2d24
Jerome Kithinji <kithinjijerome@gmail.com>
2024-05-06 01:37:02 -0500

    Service Banners hover and click styling fixed

--- src/components/serviceBanner/serviceBanner.jsx
+++ src/components/serviceBanner/serviceBanner.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
-import "./serviceBanner.scss"
+import React, { useState } from "react";
+import "./serviceBanner.scss";
 import { FaCheckCircle } from "react-icons/fa";
 
+const ServiceBanner = ({ selected, onClick }) => {
+    const [clicked, setClicked] = useState(false);
 
-const ServiceBanner = () => {
-  return (
-    <div className="serviceBanner">
+    const handleClick = () => {
+        setClicked(!clicked);
+    };
+
+    return (
+        <div className={`serviceBanner ${selected ? 'selected' : ''}`} onClick={onClick}>
         <div className="serviceBanner__headings">
-            <p className="serviceBanner__headings-title">
-                Service Name
-            </p>
-            <p className="serviceBanner__headings-subtitle">
-                We provide a wide range of services to help you achieve your goals and dreams. Whether it’s for personal or professional development.
-            </p>
-        </div>
-        <p className="serviceBanner-price">
-            £20
-        </p>
-        <div className="serviceBanner__points">
-            <div className="serviceBanner__points-container">
-                <FaCheckCircle className="serviceBanner__points-container__icon"/>
-                <div className="serviceBanner__points-container__texts">
-                    <p className="serviceBanner__points-container__texts-text">
-                        20,000+ of PNG & SVG graphics
-                    </p>
+                <p className="serviceBanner__headings-title">Service Name</p>
+                <p className="serviceBanner__headings-subtitle">
+                    We provide a wide range of services to help you achieve your
+                    goals and dreams. Whether it’s for personal or professional
+                    development.
+                </p>
+            </div>
+            <p className="serviceBanner-price">£20</p>
+            <div className="serviceBanner__points">
+                <div className="serviceBanner__points-container">
+                    <FaCheckCircle className="serviceBanner__points-container__icon" />
+                    <div className="serviceBanner__points-container__texts">
+                        <p className="serviceBanner__points-container__texts-text">
+                            20,000+ of PNG & SVG graphics
+                        </p>
+                    </div>
+                </div>
+                <div className="serviceBanner__points-container">
+                    <FaCheckCircle className="serviceBanner__points-container__icon" />
+                    <div className="serviceBanner__points-container__texts">
+                        <p className="serviceBanner__points-container__texts-text">
+                            20,000+ of PNG & SVG graphics
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div className="serviceBanner__points-container">
-                <FaCheckCircle className="serviceBanner__points-container__icon"/>
-                <div className="serviceBanner__points-container__texts">
-                    <p className="serviceBanner__points-container__texts-text">
-                        20,000+ of PNG & SVG graphics
-                    </p>
-                </div>
-            </div>
-
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default ServiceBanner
+export default ServiceBanner;
--- src/pages/servicesPage/servicesPage.jsx
+++ src/pages/servicesPage/servicesPage.jsx
@@ -1,37 +1,51 @@
-import React from 'react'
-import "./servicesPage.scss"
-import ServiceBanner from '../../components/serviceBanner/serviceBanner'
-import Faq from '../../components/faq/faq'
+import React, { useState } from "react";
+import "./servicesPage.scss";
+import ServiceBanner from "../../components/serviceBanner/serviceBanner";
+import Faq from "../../components/faq/faq";
 
 const ServicesPage = () => {
-  return (
-    <div className="servicePage">
-        <p className="servicePage-title">
-            Services & Prices
-        </p>
-        <p className="servicePage-subHeading">
-            Select a service that’s right for <span className='textHighlight'>you</span>
-        </p>
-        <div className="servicePage__banners">
-            <ServiceBanner />
-            <ServiceBanner />
-            <ServiceBanner />
-            <ServiceBanner />
-        </div>
-        <div className="servicePage__faq">
-            <p className="servicePage-title">
-                Frequently Asked Questions
+    const [selectedService, setSelectedService] = useState(null);
+
+    const handleServiceSelect = (index) => {
+        setSelectedService(index);
+    };
+    return (
+        <div className="servicePage">
+            <p className="servicePage-title">Services & Prices</p>
+            <p className="servicePage-subHeading">
+                Select a service that’s right for{" "}
+                <span className="textHighlight">you</span>
             </p>
-            <div className="servicePage__faq-container">
-                <Faq />
-                <Faq />
-                <Faq />
-                <Faq />
-                <Faq />
+            <div className="servicePage__banners">
+                <ServiceBanner
+                    selected={selectedService === 0}
+                    onClick={() => handleServiceSelect(0)}
+                />
+                <ServiceBanner
+                    selected={selectedService === 1}
+                    onClick={() => handleServiceSelect(1)}
+                />
+                <ServiceBanner
+                    selected={selectedService === 2}
+                    onClick={() => handleServiceSelect(2)}
+                />
+                <ServiceBanner
+                    selected={selectedService === 3}
+                    onClick={() => handleServiceSelect(3)}
+                />
+            </div>
+            <div className="servicePage__faq">
+                <p className="servicePage-title">Frequently Asked Questions</p>
+                <div className="servicePage__faq-container">
+                    <Faq />
+                    <Faq />
+                    <Faq />
+                    <Faq />
+                    <Faq />
+                </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default ServicesPage
+export default ServicesPage;
