3dfc574ce2e840b3dad3070a0cd4cee893887ddf
Jerome Kithinji <kithinjijerome@gmail.com>
2024-05-06 01:07:43 -0500

    Collapsed navbar closes when a link is selected

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -14,21 +14,25 @@ const NavBar = () => {
         setShowMenu(!showMenu);
     };
 
+    const closeNavbar = () => {
+        setShowMenu(false);
+    };
+
     return (
         <div className="navBar">
             <img src={Logo} alt="Slimz Detailing" className="navBar__logo" />
             <div className={`navBar__links ${showMenu ? 'showMenu' : ''}`}>
                 {/* <a href="#" className="navBar__link">Home</a> */}
-                <Link to="/" className="navBar__link">
+                <Link to="/" className="navBar__link" onClick={closeNavbar}>
                     Home
                 </Link>
-                <Link to="/services" className="navBar__link">
+                <Link to="/services" className="navBar__link" onClick={closeNavbar}>
                     Services
                 </Link>
-                <Link to="/gallery" className="navBar__link">
+                <Link to="/gallery" className="navBar__link" onClick={closeNavbar}>
                     Gallery
                 </Link>
-                <Link to="/contact" className="navBar__link">
+                <Link to="/contact" className="navBar__link" onClick={closeNavbar}>
                     Contact
                 </Link>
             </div>
